9162d2e2282512fc23d817452e0f0726dba8e2a5
nadorcott <umma.kanat@gmail.com>
2025-05-26 19:42:47 +0500

    updated url in components
    updated url

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -23,7 +23,7 @@
     methods: {
       async register() {
         try {
-          const response = await fetch("https://my-recipe-project-backend.onrender.com:10000/register", {
+          const response = await fetch("https://my-recipe-project-backend.onrender.com/register", {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
